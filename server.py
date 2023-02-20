4f572157e652d42f9adc3d750cc44a0ddc2d8f10
guilhermebrida <43304919+guilhermebrida@users.noreply.github.com>
2023-02-20 19:00:35 -0300

    teste casa

--- server.py
+++ server.py
@@ -28,18 +28,19 @@ def api_message():
         pprint(my_info)
         file_name = (my_info['head_commit']['modified'])
         # print((file_name))        
+        caminho = 'C:\\Python scripts'
+        nome_arquivo = api_message()
+        print(nome_arquivo.strip(''))
+        caminho_completo = procurar_arquivo(nome_arquivo, caminho)
+        if caminho_completo is not None:
+            print(f'O arquivo foi encontrado em: {caminho_completo}')
+        else:
+            print(f'O arquivo {nome_arquivo} não foi encontrado em {caminho}.')
         return file_name
 
 if __name__ == '__main__':
     app.run(debug=True)
-    caminho = 'C:\\Python scripts'
-    nome_arquivo = api_message()
-    print(nome_arquivo.strip(''))
-    caminho_completo = procurar_arquivo(nome_arquivo, caminho)
-    if caminho_completo is not None:
-        print(f'O arquivo foi encontrado em: {caminho_completo}')
-    else:
-        print(f'O arquivo {nome_arquivo} não foi encontrado em {caminho}.')
+
     # if api_message() is not None:
     #     cabeçalho = JENKINS_PERFIL_PURO.Json()
     #     JENKINS_PERFIL_PURO.Criar(cabeçalho)
