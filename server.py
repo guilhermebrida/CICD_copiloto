d53fe841bca2b78fbd5ae4aa805e86b302529d89
guilhermebrida <43304919+guilhermebrida@users.noreply.github.com>
2023-02-23 16:23:27 -0300

    Add files via upload

--- server.py
+++ server.py
@@ -4,8 +4,6 @@ from flask import json
 from pprint import pprint
 import re
 import JENKINS_PERFIL_PURO 
-
-
 import os
 
 def procurar_arquivo(nome_arquivo, caminho):
@@ -21,14 +19,6 @@ app = Flask(__name__)
 def api_root():
     return 'API GIT'
 
-# @app.route('/github', methods=['POST'])
-# def api_message():
-#     if request.headers['Content-Type'] == 'application/json':
-#         my_info = request.json
-#         pprint(my_info)
-#         file_name = (my_info['head_commit']['modified'])
-#         print((file_name))        
-#         return file_name
 
 @app.route('/github', methods=['POST'])
 def api_message():
@@ -42,27 +32,12 @@ def api_message():
         if caminho_completo is not None:
             print(f'O arquivo foi encontrado em: {caminho_completo}')
             JENKINS_PERFIL_PURO.GeraPerfil().main(caminho_completo)
-
-
         else:
             print(f'O arquivo {file_name} não foi encontrado em {caminho}.')
         return file_name
 
 if __name__ == '__main__':
     app.run(debug=True)
-    # caminho = 'C:\\Python scripts'
-    # nome_arquivo = api_message()
-    # print(nome_arquivo.strip(''))
-    # caminho_completo = procurar_arquivo(nome_arquivo, caminho)
-    # if caminho_completo is not None:
-    #     print(f'O arquivo foi encontrado em: {caminho_completo}')
-    # else:
-    #     print(f'O arquivo {nome_arquivo} não foi encontrado em {caminho}.')
-    # if api_message() is not None:
-    #     cabeçalho = JENKINS_PERFIL_PURO.Json()
-    #     JENKINS_PERFIL_PURO.Criar(cabeçalho)
-    #     comandos=JENKINS_PERFIL_PURO.message()
-    #     AES_pkcs5_obj= JENKINS_PERFIL_PURO.AES_pkcs5(comandos)
-    #     encrypted_message = AES_pkcs5_obj.encrypt(comandos)
+
 
 
